119fba4435a661450b6cb036e7820bca3678ca0c
Donnahue George <Donnahuegjr@gmail.com>
2021-01-13 11:24:31 -0500

    got to print results

--- index.js
+++ index.js
@@ -5,57 +5,61 @@ const util = require("util");
 const writeReadMe = util.promisify(fs.writeFile);
 
 const readMePrompt = () =>
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is the title of the project?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "Provide me a description of your project.",
-      name: "desc",
-    },
-    {
-      type: "input",
-      message: "What are the installation instructions?",
-      name: "install",
-    },
-    {
-      type: "input",
-      message: "Any usage information?",
-      name: "usage",
-    },
-    {
-      type: "input",
-      message: "Who are the contributers to the project?",
-      name: "contr",
-    },
-    {
-      type: "input",
-      message: "What are the test instruction if any?",
-      name: "test",
-    },
-    {
-      type: "list",
-      message: "Which license does your project has?",
-      name: "license",
-      choices: [
-        "MIT",
-        "Mozilla",
-        "Boost",
-        "Apache",
-        "BSD",
-        "Eclipse",
-        "GNU",
-        "IBM",
-        "Perl",
-        "Unlicense",
-      ],
-    },
-    {
-      type: "input",
-      message: "What is the title of the project?",
-      name: "title",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the title of the project?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "Provide me a description of your project.",
+        name: "desc",
+      },
+      {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "install",
+      },
+      {
+        type: "input",
+        message: "Any usage information?",
+        name: "usage",
+      },
+      {
+        type: "input",
+        message: "Who are the contributers to the project?",
+        name: "contr",
+      },
+      {
+        type: "input",
+        message: "What are the test instruction if any?",
+        name: "test",
+      },
+      {
+        type: "list",
+        message: "Which license does your project has?",
+        name: "license",
+        choices: [
+          "MIT",
+          "Mozilla",
+          "Boost",
+          "Apache",
+          "BSD",
+          "Eclipse",
+          "GNU",
+          "IBM",
+          "Perl",
+          "Unlicense",
+        ],
+      },
+      {
+        type: "input",
+        message: "ghg",
+        name: "joe",
+      },
+    ])
+    .then((post) => console.log(post.license));
+
+readMePrompt();

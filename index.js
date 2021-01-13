4c93fa72b7916b7d016bb46eb98ea164974aa0b1
Donnahue George <Donnahuegjr@gmail.com>
2021-01-13 11:01:27 -0500

    first question done

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeReadMe = util.promisify(fs.writeFile);
+
+const readMePrompt = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the title of the project?",
+      name: "title",
+    },
+    {},
+  ]);

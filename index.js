70bc07e6416790bc2f9013f10b58c193a8ab1a0d
Donnahue George <Donnahuegjr@gmail.com>
2021-01-13 12:08:12 -0500

    added string literal const

--- index.js
+++ index.js
@@ -5,66 +5,65 @@ const util = require("util");
 const writeReadMe = util.promisify(fs.writeFile);
 
 const readMePrompt = () =>
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What is the title of the project?",
-        name: "title",
-      },
-      {
-        type: "input",
-        message: "Provide me a description of your project.",
-        name: "desc",
-      },
-      {
-        type: "input",
-        message: "What are the installation instructions?",
-        name: "install",
-      },
-      {
-        type: "input",
-        message: "Any usage information?",
-        name: "usage",
-      },
-      {
-        type: "input",
-        message: "Who are the contributers to the project?",
-        name: "contr",
-      },
-      {
-        type: "input",
-        message: "What are the test instruction if any?",
-        name: "test",
-      },
-      {
-        type: "list",
-        message: "Which license does your project has?",
-        name: "license",
-        choices: [
-          "MIT",
-          "Mozilla",
-          "Boost",
-          "Apache",
-          "BSD",
-          "Eclipse",
-          "GNU",
-          "IBM",
-          "Perl",
-          "Unlicense",
-        ],
-      },
-      {
-        type: "input",
-        message: "What is your Github username?",
-        name: "username",
-      },
-      {
-        type: "input",
-        message: "What is your email address?",
-        name: "email",
-      },
-    ])
-    .then((post) => console.log(post));
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the title of the project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Provide me a description of your project.",
+      name: "desc",
+    },
+    {
+      type: "input",
+      message: "What are the installation instructions?",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "Any usage information?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Who are the contributers to the project?",
+      name: "contr",
+    },
+    {
+      type: "input",
+      message: "What are the test instruction if any?",
+      name: "test",
+    },
+    {
+      type: "list",
+      message: "Which license does your project has?",
+      name: "license",
+      choices: [
+        "MIT",
+        "Mozilla",
+        "Boost",
+        "Apache",
+        "BSD",
+        "Eclipse",
+        "GNU",
+        "IBM",
+        "Perl",
+        "Unlicense",
+      ],
+    },
+    {
+      type: "input",
+      message: "What is your Github username?",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ]);
+const ReadMeString = (sentence) => ``;
 
-readMePrompt();
+readMePrompt().then((post) => console.log(post));
